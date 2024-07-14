038de643691a590425e5e5f03bdf5e858ffc4f9f
Cristian G <crttngr@gmail.com>
2024-07-14 18:36:36 +0200

    Adding new features and refactoring
    Added a welcome card when you join as a member.
    Added a user/bot counter

--- config.py
+++ config.py
@@ -1,6 +1,15 @@
 class Config:
-    role_name_new_user = "Sin Verificar"
-    role_name_verified = "Verificado"
-    message_id = "1259275517266366485"
-    prefix = "."
-    channel_id = "1259275025866031134"
+    # ID Servidor
+    guild_id = 1259274950867423232 # ID del servidor
+
+    # Configuracion del BOT
+    prefix = "." # Prefijo para los comandos del bot
+    role_name_new_user = "Sin Verificar" # Nombre del rol por defecto al entrar al servidor
+    role_name_verified = "Verificado"    # Nombre deñ rol al verificar la cuenta 
+    channel_normas_id = "1259309455955198043" # Id del canal de normas
+    message_normas_id = "1259509578274574406" # Id del mensaje para verificar la cuenta 
+
+    # Counter Users 
+    counter_member_channel_id = 1259480338347065396 # ID Canal de voz "Miembros"
+    counter_active_member_channel_id = 1262073603604877343 # ID Canal de voz "Activos"
+    counter_bot_channel_id = 1259502736085614756 # ID Canal de voz "Bots"
--- main.py
+++ main.py
@@ -1,105 +1,209 @@
 import os
 from dotenv import load_dotenv
-import urllib.request
-import json
 from config import Config
 import discord
-from discord.ext import commands
+from easy_pil import Editor, load_image_async, Font
+from discord.ext import commands, tasks
 
-
-# Configuración inicial
-
+# Obtinen las configuraciones necesarias
 load_dotenv()
 token = os.getenv('bot_token')
-
 prefix = Config.prefix
-
 bot = commands.Bot(command_prefix=prefix, intents=discord.Intents.all())
 
 # Variables de configuración del bot
 bot.setup = False
 bot.join_role_name = Config.role_name_new_user
 bot.verify_role_name = Config.role_name_verified
-bot.message_id = Config.message_id
-bot.channel_id = Config.channel_id
+bot.message_normas_id = Config.message_normas_id
+bot.channel_normas_id = Config.channel_normas_id
 
+# Conección incial con el bot
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user.name}")
+    countUsers.start()
 
+# Inicia el setup del bot
 @bot.command()
 async def setup(ctx):
     try:
-        message_id = int(bot.message_id)
-        channel_id = int(bot.channel_id)
+        message_id = int(bot.message_normas_id)
+        channel_id = int(bot.channel_normas_id)
     except ValueError:
-        return await ctx.send("Invalid Message ID or Channel ID passed")
+        return await ctx.send("ID del canal o del mensaje erroneo")
     except Exception as e:
-        return await ctx.send(f"An error occurred: {e}")
+        return await ctx.send(f"Error en la configuración: {e}")
 
     channel = bot.get_channel(channel_id)
     if channel is None:
-        return await ctx.send("Channel Not Found")
+        return await ctx.send("Canal no encontado")
 
     try:
         message = await channel.fetch_message(message_id)
     except discord.NotFound:
-        return await ctx.send("Message Not Found")
+        return await ctx.send("Mensaje no encontrado")
     except Exception as e:
-        return await ctx.send(f"An error occurred: {e}")
+        return await ctx.send(f"Error al encontrar el mensaje: {e}")
     
     try:
         await message.add_reaction("✅")
     except Exception as e:
-        return await ctx.send(f"Failed to add reaction: {e}")
+        return await ctx.send(f"Error al añadir la reacción: {e}")
 
-    await ctx.send("Setup Successful 🟢")
     bot.setup = True
+    print("Configuración del bot compeltada")
+    await ctx.send("Configuración compeltada 🟢")
 
+# Asigna el rol "Sin verificar" a los nuevos usuarios y envía una tarjeta de binevenida
+@bot.event
+async def on_member_join(member):
+    channel = member.guild.system_channel
+    background = Editor("images\\pic1.jpg")
+
+    profile_image = None
+    # URL de un avatar predeterminado de Discord
+    default_avatar = 'https://cdn.discordapp.com/embed/avatars/0.png'
+
+    # Verificar si el usuario tiene un avatar; de lo contrario, usar un avatar predeterminado
+    if member.avatar:
+        profile_image = await load_image_async(str(member.avatar.url))
+    else:
+        profile_image = await load_image_async(default_avatar)
+
+    profile = Editor(profile_image).resize((150, 150)).circle_image()
+    poppins = Font.poppins(size=50, variant="bold")
+    poppins_small = Font.poppins(size=20, variant="light")
+
+    background.paste(profile, (325, 90))
+    background.ellipse((325, 90), 150, 150, outline="white", stroke_width=5)
+    
+    background.text((400, 260), f"BIENVENIDO A {member.guild.name}", color="white", font=poppins, align="center")
+    background.text((400, 325), f"{member.name}", color="white", font=poppins_small, align="center")
+    
+    file = discord.File(fp=background.image_bytes, filename="pic1.jpg")
+
+    await channel.send(f"Hola {member.mention}! Binevenido a **{member.guild.name}** Para mas información ve a **#Normas**")
+    await channel.send(file=file)
+
+    #  Asigna el rol "Sin Verificar" a los nuevos usuarios
+    guild = member.guild
+    role = discord.utils.get(guild.roles, name=bot.join_role_name)
+
+    # Comprueba que el rol exista 
+    if role is None:
+        return print(f"No se ha encontrado el rol:'{bot.join_role_name}'")
+    
+    # Agrega el rol "Sin Verificar"
+    try:
+        await member.add_roles(role)
+        print(f"Rol '{role.name}' asignado a {member.name}")
+    except discord.Forbidden:
+        print(f"Error al añadir el rol, verifica los permisos para {member.name}")
+    except Exception as e:
+        print(f"Error al agregar el rol: {e}")
+
+# Obtiene la reacción de los nuevos usuarios ante las normas y agrega el rol "Verificado", tambien elimina el rol "Sin Verificar"
 @bot.event
 async def on_raw_reaction_add(payload):
     if not bot.setup:
-        return print(f"Bot is not set up. Type {prefix}setup to set up the bot")
+        return print(f"El bot no está configurado. Escribe {prefix}setup para configurarlo")
 
-    if payload.message_id == int(bot.message_id) and str(payload.emoji) == "✅":
+    if payload.message_id == int(bot.message_normas_id) and str(payload.emoji) == "✅":
         guild = bot.get_guild(payload.guild_id)
         if guild is None:
-            return print("Guild Not Found. Terminating Process")
+            return print("No se ha encotrado el servidor")
         
         role = discord.utils.get(guild.roles, name=bot.verify_role_name)
         old_role = discord.utils.get(guild.roles, name=bot.join_role_name)
         if role is None:
-            return print("Role Not Found. Terminating Process")
+            return print("El rol no se ha encotrado")
         
         member = guild.get_member(payload.user_id)
         if member is None:
             return
         
+        # Añadir/Eliminar roles
         try:
             if old_role in member.roles:
+                # Elimina el rol "Sin Verificar"
                 await member.remove_roles(old_role)
-                print(f"Removed old role '{old_role.name}' from {member.name}")
+                print(f"Rol eliminado '{old_role.name}' a {member.name}")
+            # Agrega el rol "Verificado"
             await member.add_roles(role)
-            print(f"Assigned role '{role.name}' to {member.name}")
+            print(f"Rol asignado '{role.name}' a {member.name}")
         except discord.Forbidden:
-            print(f"Failed to add/remove role: Missing Permissions for {member.name}")
+            print(f"Permisos inecesarios para añadir/remover el rol a {member.name}")
         except Exception as e:
-            print(f"Failed to add/remove role: {e}")
+            print(f"Error al añadir/remover el rol: {e}")
 
-@bot.event
-async def on_member_join(member):
-    guild = member.guild
-    role = discord.utils.get(guild.roles, name=bot.join_role_name)
-    
-    if role is None:
-        return print(f"Role '{bot.join_role_name}' not found. Ensure the role exists.")
-    
-    try:
-        await member.add_roles(role)
-        print(f"Assigned role '{role.name}' to {member.name}")
-    except discord.Forbidden:
-        print(f"Failed to add role: Missing Permissions for {member.name}")
-    except Exception as e:
-        print(f"Failed to add role: {e}")
+# Obtiene la cantidad de miembros y bots del discord y la muestra como nombre de canales de voz 
+@tasks.loop(minutes=5)
+async def countUsers():
+
+    # Obtiene el servidor y los canales apartir del id
+    guild_id = bot.get_guild(Config.guild_id)
+    member_channel_id = bot.get_channel(Config.counter_member_channel_id)
+    active_channel_id = bot.get_channel(Config.counter_active_member_channel_id)
+    bot_cannle_id = bot.get_channel(Config.counter_bot_channel_id)
+
+    # Comprueba que exista el servidor
+    if guild_id is not None:
+        totalMembers = 0
+        activeMembers = 0
+        totalBots = 0
+
+        # Cuenta los usuarios
+        for member in guild_id.members:
+            if not member.bot:
+                totalMembers += 1
+        # Cuenta los usuarios conectados
+            if not member.bot and member.status != discord.Status.offline:
+                activeMembers += 1
+        # Cuenta los bots
+            if member.bot:
+                totalBots += 1
+    else:
+        print(f"No se ha encontrado el servidor con id: {guild_id}.")
+        return  
+
+    # Modifica el nombre del canal de voz "Miembros"
+    if member_channel_id:
+        # Verificar permisos para editar el canal
+        if member_channel_id.permissions_for(guild_id.me).manage_channels:
+            try:
+                await member_channel_id.edit(name=f"【 👥 】 Miembros: {totalMembers}")
+            except discord.Forbidden:
+                print(f"Permisos inecesarios para editar el canal con id: {member_channel_id.name}")
+        else:
+            print(f"El bot no tiene permisos para editar canales")
+    else:
+        print(f"No se ha encontrado el canal con id: {member_channel_id}")
+
+    # Modifica el nombre del canal de voz "Activos"
+    if active_channel_id:
+        # Verificar permisos para editar el canal
+        if active_channel_id.permissions_for(guild_id.me).manage_channels:
+            try:
+                await active_channel_id.edit(name=f"【 🟢 】 Activos: {activeMembers}")
+            except discord.Forbidden:
+                print(f"Permisos inecesarios para editar el canal con id: {active_channel_id.name}")
+        else:
+            print(f"El bot no tiene permisos para editar canales")
+    else:
+        print(f"No se ha encontrado el canal con id: {active_channel_id}")
+
+    # Modifica el nombre del canal de voz "Bots"
+    if bot_cannle_id:
+        # Verificar permisos para editar el canal
+        if bot_cannle_id.permissions_for(guild_id.me).manage_channels:
+            try:
+                await bot_cannle_id.edit(name=f"【 🤖 】 Bots: {totalBots}")
+            except discord.Forbidden:
+                print(f"Permisos inecesarios para editar el canal con id: {bot_cannle_id.name}")
+        else:
+            print(f"El bot no tiene permisos para editar canales")
+    else:
+        print(f"No se ha encontrado el canal con id: {bot_cannle_id}")
 
 bot.run(token)
